cf562b7fff7d89b5dd1731aee3e9585b43b339c0
Jole34 <jovan.glavonja85@gmail.com>
2020-04-03 22:08:16 +0200

    first

---
+++ src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.util.ArrayList;
+
+public class Controller {
+    @FXML
+    private PasswordField pass;
+    @FXML
+    private TextField log;
+
+
+    public ArrayList<String> collectDataLog(){
+
+        return null;
+    }
+
+
+
+}
