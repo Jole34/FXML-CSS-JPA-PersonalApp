d47e55cfaa79e078de9766f2336be786990428f6
Jole34 <jovan.glavonja85@gmail.com>
2020-04-03 22:38:42 +0200

    Alert

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -13,9 +14,19 @@ public class Controller {
     @FXML
     private TextField log;
 
+    int br =0;
 
     public ArrayList<String> collectDataLog(){
-
+        ++br;
+        String passtext = pass.getText();
+        String logname = log.getText();
+        Alert a = new Alert(Alert.AlertType.ERROR);
+        a.setContentText("Неисправно корисничко име или шифра ");
+        a.setTitle("Грешка!");
+        a.setHeaderText("Покушај број: "+br+"!");
+        if (br ==3)
+            a.setHeaderText("Ако ниси Јован стоп!");
+        a.show();
         return null;
     }
 
