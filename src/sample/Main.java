f8162910040ac9b8dd6adb55c349350e20048b92
Jole34 <jovan.glavonja85@gmail.com>
2020-04-04 19:58:47 +0200

    test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,11 +11,19 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root2 = FXMLLoader.load(getClass().getResource("sample2.fxml"));
+
         Scene scene = new Scene(root, 800, 500);
         primaryStage.setTitle("Распоред");
         scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        Stage stage2 = new Stage();
+        Scene scene2 = new Scene(root2, 800, 500);
+        scene2.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
+        stage2.setScene(scene2);
+        stage2.show();
     }
 
     public static void main(String[] args) {
